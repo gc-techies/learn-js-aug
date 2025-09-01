83cd977cd8547cc4fed30d7c74500d2575f93b1f
gc-techies <geecomptech@gmail.com>
2025-09-01 14:10:58 +0100

    revision

--- js-intro.js
+++ js-intro.js
@@ -4,10 +4,30 @@ var practice;
 practice = 71;
 // variable assignment
 practice = 21;
+// variable declaration + initialization
+var nos = 5;
 // variable declaration + initialization done smartly
 let dell = 98;
 var let = 5;
 console.log('Mr. Adeleye is welcome to js and he looks terrified');
+
+/*  rules
+    1.  variable names cannot be a reserved keyword (e.g if, not)
+    2.  cannot start with a number
+    3.  cannot contain space or hyphen
+*/
+
+/*  good practices
+    1.  should be meaningful
+*/
+
+/*  note
+    1.  variable names are case sensitive
+*/
+
+/*  Primitive types
+    String, Number, Boolean, undefined and null
+*/
 
 // Camel Notation
 // String literal
@@ -25,8 +45,10 @@ console.log('primitives and reference/composite');
 console.log('number, boolean, string, undefined, null');
 // number data type using Number literal syntax
 let age = 45;
+let strAge = '45';
 // boolean data type using Boolean literal syntax
 let isAdult = false;
+let strIsAdult = 'false';
 // undefined data tuype
 let x = undefined;
 let firstName = null;
