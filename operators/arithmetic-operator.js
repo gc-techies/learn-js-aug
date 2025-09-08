8399746131c69a4336f4aba20094791962f337f9
gc-techies <geecomptech@gmail.com>
2025-09-08 13:09:57 +0100

    arithmetic operators

---
+++ operators/arithmetic-operator.js
@@ -0,0 +1,32 @@
+let a = 5;
+let b = 3;
+
+// addition operator
+console.log(a + b);
+// subtraction operator
+console.log(b - a);
+// multiplication operator
+console.log(a * b);
+// division operator
+console.log(a / b);
+// modulus operator
+console.log(a % b);
+console.log(4 % 8);
+// pow operator
+console.log(a ** b);
+
+let account_balance = 9;
+account_balance = account_balance - a;
+console.log('account bal', account_balance);
+
+account_balance = account_balance * 3;
+console.log('account bal after multiplication', account_balance);
+
+let c = 5;
+console.log('decrement before', --c);
+let e = 5;
+console.log('decrement after', e--);
+console.log('after decrement', e);
+
+
+let d = 8;
