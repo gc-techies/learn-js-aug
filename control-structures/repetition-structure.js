1fc02e0ce8c799c74eb5c48db08b26237b05c1be
gc-techies <geecomptech@gmail.com>
2025-09-22 10:28:27 +0100

    nested loop, string concatenation, break and continue statements

--- control-structures/repetition-structure.js
+++ control-structures/repetition-structure.js
@@ -7,11 +7,33 @@
         //  codes to be repeated
     }
 */
-
-for(let count = 1; count <= 100; count += 1){
+let count = 1;
+for(; ; ){
+    if(count > 100){
+        break;
+    }
     if(count % 2 === 0){
         console.log(count ,' is even');
     }else {
+        // continue;
         console.log(count, 'is odd');
     }
+    count += 1;
+}
+
+// STRING CONCATENATION
+let a = 'my name';
+let b = ' is ibrahim';
+let d = 'i am hungry';
+d += ', but i will be fine';
+let c = a + b;
+console.log(d);
+
+// NESTED LOOP
+for(let num = 1; num <= 5; num++){
+    let stars = '';
+    for(let inner = 1; inner <= num; inner++){
+        stars += "*";
+    }
+    console.log(stars);
 }
