ef2e4f59ebbbb9ee0217a5ede2a9d809eacef9d7
gc-techies <geecomptech@gmail.com>
2026-03-23 12:22:18 +0100

    intro functions continued

--- funciton/intro.js
+++ funciton/intro.js
@@ -1,3 +1,5 @@
+sum();
+add(2); // supply necessary arguments
 // function declaration
 function sum(){
     let a = 4;
@@ -13,6 +15,20 @@ function add(a, b){
     console.log(sum);
 }
 
+// function expression
+const fnOne = function find(){
+    console.log('hello from function expression one');
+}
+
+const fnTwo = function (){
+    console.log('hello from function expression two using anonymous function');
+}
+
+fnOne(); // call the function expression
+fnTwo(); // call the function expression
 // function call
-sum();
-add(2, 40);
+
+let z = 5;
+console.log('value of z', z);
+
+// HOISTING
